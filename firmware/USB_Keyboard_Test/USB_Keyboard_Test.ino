6c567c663c2f98ba89e923ac9ac40828bcdbd395
Liam M <ArchimedesPi@users.noreply.github.com>
2014-09-09 13:38:48 -0500

    Courtesy period, delay between counter prints

--- firmware/USB_Keyboard_Test/USB_Keyboard_Test.ino
+++ firmware/USB_Keyboard_Test/USB_Keyboard_Test.ino
@@ -3,11 +3,14 @@ int counter = 0;                  // counter
 void setup() {
   // initialize control over the keyboard:
   Keyboard.begin();
+  delay(1000);
 }
 
 void loop() {
     // increment the button counter
     counter++;
     Keyboard.println(counter);
+    
+    delay(100);
 }
 
